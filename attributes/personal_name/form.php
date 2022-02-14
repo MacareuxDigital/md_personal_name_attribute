7d0a628f7c438f2a500606629abb9e05750aba9f
Takuro Hishikawa <hishikawa@concrete5.co.jp>
2022-02-15 05:08:42 +0900

    Fix bug: empty pattern attribute blocks all input

--- attributes/personal_name/form.php
+++ attributes/personal_name/form.php
@@ -13,8 +13,15 @@ $given_name = isset($given_name) ? $given_name : null;
 $family_name_label = isset($akFamilyNameLabel) ? $akFamilyNameLabel : t('Family Name');
 $given_name_label = isset($akGivenNameLabel) ? $akGivenNameLabel : t('Given Name');
 $akFirstName = isset($akFirstName) ? $akFirstName : 'family_name';
-$family_name_pattern = isset($akFamilyNamePattern) ? $akFamilyNamePattern : '';
-$given_name_pattern = isset($akGivenNamePattern) ? $akGivenNamePattern : '';
+
+$family_name_options = [];
+if (isset($akFamilyNamePattern) && $akFamilyNamePattern) {
+    $family_name_options['pattern'] = $akFamilyNamePattern;
+}
+$given_name_options = [];
+if (isset($akGivenNamePattern) && $akGivenNamePattern) {
+    $given_name_options['pattern'] = $akGivenNamePattern;
+}
 ?>
 
 <div class="form-inline ccm-attribute-personal-name-composer-wrapper">
@@ -22,19 +29,19 @@ $given_name_pattern = isset($akGivenNamePattern) ? $akGivenNamePattern : '';
     <?php if ($akFirstName === 'family_name') { ?>
     <div class="form-group ccm-attribute-personal-name-family-name">
         <?= $form->label($this->field('family_name'), $family_name_label); ?>
-        <?= $form->text($this->field('family_name'), $family_name, ['pattern' => $family_name_pattern]); ?>
+        <?= $form->text($this->field('family_name'), $family_name, $family_name_options); ?>
     </div>
     <?php } ?>
 
     <div class="form-group ccm-attribute-personal-name-given-name">
         <?= $form->label($this->field('given_name'), $given_name_label); ?>
-        <?= $form->text($this->field('given_name'), $given_name, ['pattern' => $given_name_pattern]); ?>
+        <?= $form->text($this->field('given_name'), $given_name, $given_name_options); ?>
     </div>
 
     <?php if ($akFirstName !== 'family_name') { ?>
         <div class="form-group ccm-attribute-personal-name-family-name">
             <?= $form->label($this->field('family_name'), $family_name_label); ?>
-            <?= $form->text($this->field('family_name'), $family_name, ['pattern' => $family_name_pattern]); ?>
+            <?= $form->text($this->field('family_name'), $family_name, $family_name_options); ?>
         </div>
     <?php } ?>
 
