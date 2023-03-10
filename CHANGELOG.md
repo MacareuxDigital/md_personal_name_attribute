4658de979d720605252124c955a46d1ed7b9cb53
Takuro Hishikawa <hishikawa@macareux.co.jp>
2023-03-10 12:48:41 +0900

    Removed buggy pattern attribute

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Release Notes
 
+## [v1.0.1 (Mar 9, 2023)]
+
+- Removed buggy pattern attribute
+
 ## [v1.0 (May 22, 2022)]
 
 - Initial Release.
--- attributes/personal_name/form.php
+++ attributes/personal_name/form.php
@@ -13,15 +13,6 @@ $given_name = isset($given_name) ? $given_name : null;
 $family_name_label = isset($akFamilyNameLabel) ? $akFamilyNameLabel : t('Family Name');
 $given_name_label = isset($akGivenNameLabel) ? $akGivenNameLabel : t('Given Name');
 $akFirstName = isset($akFirstName) ? $akFirstName : 'family_name';
-
-$family_name_options = [];
-if (isset($akFamilyNamePattern) && $akFamilyNamePattern) {
-    $family_name_options['pattern'] = $akFamilyNamePattern;
-}
-$given_name_options = [];
-if (isset($akGivenNamePattern) && $akGivenNamePattern) {
-    $given_name_options['pattern'] = $akGivenNamePattern;
-}
 ?>
 
 <div class="row ccm-attribute-personal-name-composer-wrapper">
@@ -29,19 +20,19 @@ if (isset($akGivenNamePattern) && $akGivenNamePattern) {
     <?php if ($akFirstName === 'family_name') { ?>
         <div class="col-md-6 form-group ccm-attribute-personal-name-family-name">
             <?= $form->label($this->field('family_name'), $family_name_label); ?>
-            <?= $form->text($this->field('family_name'), $family_name, $family_name_options); ?>
+            <?= $form->text($this->field('family_name'), $family_name); ?>
         </div>
     <?php } ?>
 
     <div class="col-md-6 form-group ccm-attribute-personal-name-given-name">
         <?= $form->label($this->field('given_name'), $given_name_label); ?>
-        <?= $form->text($this->field('given_name'), $given_name, $given_name_options); ?>
+        <?= $form->text($this->field('given_name'), $given_name); ?>
     </div>
 
     <?php if ($akFirstName !== 'family_name') { ?>
         <div class="col-md-6 form-group ccm-attribute-personal-name-family-name">
             <?= $form->label($this->field('family_name'), $family_name_label); ?>
-            <?= $form->text($this->field('family_name'), $family_name, $family_name_options); ?>
+            <?= $form->text($this->field('family_name'), $family_name); ?>
         </div>
     <?php } ?>
 
